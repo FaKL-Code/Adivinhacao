fe371a6e2a3c7a7ea871554a9034c0a1e61ace69
Julio Facal <starlyn.dev@gmail.com>
2022-10-12 23:45:14 -0300

    Sistema de pontos

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,6 +5,10 @@ print("-----------------------------")
 
 numero = randint(0, 100)
 
+rodada = 1
+
+pontos = 1000
+
 print("Qual o nível de dificuldade?")
 print("1- Fácil  2- Médio  3- Difícil")
 
@@ -12,12 +16,12 @@ dificuldade = int(input(""))
 
 if dificuldade == 1:
     tentativas = 10
+    
 elif dificuldade == 2:
     tentativas = 7
+    
 elif dificuldade == 3:
     tentativas = 5
-
-rodada = 1
 
 for rodada in range (1, tentativas + 1):
     
@@ -45,8 +49,11 @@ for rodada in range (1, tentativas + 1):
 
         if maior:
             print("O número é menor que isso")
+            pontos = pontos - (valor_inserido - numero)
 
         elif menor:
             print("O número é maior")
+            pontos = pontos - (numero - valor_inserido)
     
 print("O número era: ", numero)
+print("Você pontuou: {} pontos".format(pontos))
