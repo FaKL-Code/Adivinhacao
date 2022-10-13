29db89d2b0a0c6f16dad2e605c7f0fee0aa99ef6
Julio Facal <starlyn.dev@gmail.com>
2022-10-13 10:12:44 -0300

    Aprimoramento logica da forca

--- forca.py
+++ forca.py
@@ -5,8 +5,24 @@ def start():
     print("******* Jogo da Forca! *******")
     print("*********************************")
 
-
-
+    palavra = "banana"
+    
+    enforcou = False
+    
+    acertou = False
+    
+    while not enforcou and not acertou:
+        print("Jogando!")
+        
+        palpite = input("Escolha uma letra: ").strip
+        
+        pos = 0
+        
+        for letra in palavra:
+            if(palpite == letra):
+                print("Temos a letra {} na posição {}!".format(letra, pos))
+            pos += 1
+        
     print("Fim do jogo!")
     
 if (__name__ == "__main__"):
